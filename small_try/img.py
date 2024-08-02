1c9f63b4000996a4231c8229eafb88e056929126
admingaoxin <577882797@qq.com>
2024-08-02 15:44:19 +0800

    update

--- small_try/img.py
+++ small_try/img.py
@@ -1,17 +1,27 @@
 import os
 from PIL import Image
 
+
 def resize_images(directory, new_size):
-    for filename in os.listdir(directory):
-        if filename.endswith((".jpg", ".jpeg", ".png", ".bmp")):
-            file_path = os.path.join(directory, filename)
-            with Image.open(file_path) as img:
-                resized_img = img.resize(new_size)
-                resized_img.save(file_path)
-                print(f"已调整 {filename} 的大小")
+    for filename in os.listdir (directory):
+        if filename.endswith ((".jpg", ".jpeg", ".png", ".bmp")):
+            file_path = os.path.join (directory, filename)
+            with Image.open (file_path) as img:
+                # 使用LANCZOS重采样方法进行高质量调整
+                resized_img = img.resize (new_size, Image.LANCZOS)
+
+                # 生成新的文件名，保留原始文件
+                name, ext = os.path.splitext (filename)
+                new_filename = f"{name}_resized{ext}"
+                new_file_path = os.path.join (directory, new_filename)
+
+                # 保存调整后的图片
+                resized_img.save (new_file_path, quality=100)
+                print (f"已调整 {filename} 的大小并保存为 {new_filename}")
+
 
 # 使用示例
 directory = "D:/wallpapers/deepnodezq-mas"  # 替换为您的图片目录路径
 new_size = (512, 512)  # 设置新的像素大小，这里是宽800像素，高600像素
 
-resize_images(directory, new_size)
+resize_images (directory, new_size)
--- socket/demo1.py
+++ socket/demo1.py
@@ -1,48 +1,102 @@
+import os
 from scapy.all import *
 import time
 import optparse
 import datetime
-
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 nowtime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 print(nowtime)
-# 回调打印函数
-def PackCallBack(packet, hostIP):
-    print('*' * 30)
-    # 打印源IP，源端口，目的IP，目的端口
-    direction = "Outgoing" if packet[IP].src == hostIP else "Incoming"
-    print("[%s] %s: %s:%s ----> %s:%s" % (TimeStamp2Time(packet.time), direction, packet[IP].src, packet.sport, packet[IP].dst, packet.dport))
-    # 打印数据包
-    print('packet',packet.show())
-    # 打印协议内容
-    if Raw in packet:
-        print("Protocol Content: ", packet[Raw].load)
-    print('*' * 30)
 
+# 回调打印函数
+def PackCallBack(packet, hostIP, ui, writer):
+    direction = "Outgoing" if packet[IP].src == hostIP else "Incoming"
+    summary = "[%s] %s: %s:%s ----> %s:%s" % (TimeStamp2Time(packet.time), direction, packet[IP].src, packet.sport, packet[IP].dst, packet.dport)
+    details = packet.show(dump=True)
+    if Raw in packet:
+        details += "\nProtocol Content: " + str(packet[Raw].load)
+    ui.packet_received.emit(summary, details)
+    writer.write(packet)
 
 # 时间戳转换函数
 def TimeStamp2Time(timeStamp):
-    timeTmp = time.localtime(timeStamp)  # time.localtime()格式化时间戳为本地时间
-    myTime = time.strftime("%Y-%m-%d %H:%M:%S", timeTmp)  # 将本地时间格式化为字符串
+    timeTmp = time.localtime(timeStamp)
+    myTime = time.strftime("%Y-%m-%d %H:%M:%S", timeTmp)
     return myTime
 
+class PacketSnifferUI(QtWidgets.QWidget):
+    packet_received = QtCore.pyqtSignal(str, str)
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.packet_received.connect(self.add_packet)
+        self.sniffer_thread = None
+
+    def initUI(self):
+        self.setWindowTitle('Packet Sniffer')
+        self.setGeometry(100, 100, 800, 600)
+
+        self.ipLabel = QtWidgets.QLabel('IP Address:', self)
+        self.ipInput = QtWidgets.QLineEdit(self)
+        self.ipInput.setText('39.98.196.51')
+
+        self.startButton = QtWidgets.QPushButton('Start Sniffing', self)
+        self.startButton.clicked.connect(self.toggle_sniffing)
+
+        self.packetList = QtWidgets.QListWidget(self)
+        self.packetDetails = QtWidgets.QTextEdit(self)
+        self.packetDetails.setReadOnly(True)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.ipLabel)
+        layout.addWidget(self.ipInput)
+        layout.addWidget(self.startButton)
+        layout.addWidget(self.packetList)
+        layout.addWidget(self.packetDetails)
+        self.setLayout(layout)
+
+        self.packetList.itemClicked.connect(self.display_packet_details)
+
+    def toggle_sniffing(self):
+        if self.sniffer_thread and self.sniffer_thread.isRunning():
+            self.sniffer_thread.terminate()
+            self.sniffer_thread = None
+            self.startButton.setText('Start Sniffing')
+        else:
+            self.packetList.clear()
+            self.packetDetails.clear()
+            hostIP = self.ipInput.text()
+            defFilter = "host " + hostIP
+            fileName = f'{nowtime}.pcap'
+            self.sniffer_thread = SnifferThread(defFilter, hostIP, self, fileName)
+            self.sniffer_thread.start()
+            self.startButton.setText('Stop Sniffing')
+
+    def add_packet(self, summary, details):
+        item = QtWidgets.QListWidgetItem(summary)
+        item.setData(QtCore.Qt.UserRole, details)
+        self.packetList.addItem(item)
+
+    def display_packet_details(self, item):
+        details = item.data(QtCore.Qt.UserRole)
+        self.packetDetails.setPlainText(details)
+
+class SnifferThread(QtCore.QThread):
+    def __init__(self, filter, hostIP, ui, fileName):
+        super().__init__()
+        self.filter = filter
+        self.hostIP = hostIP
+        self.ui = ui
+        self.fileName = fileName
+
+    def run(self):
+        writer = PcapWriter(self.fileName, append=True, sync=True)
+        while True:
+            packets = sniff(filter=self.filter, prn=lambda x: PackCallBack(x, self.hostIP, self.ui, writer), count=5)
+
 if __name__ == '__main__':
-    parser = optparse.OptionParser("Example:python %prog -i 39.98.196.51 -c 5 -o {nowtime}.pcap\n")
-    # 添加IP参数 -i
-    parser.add_option('-i', '--IP', dest='hostIP', default='39.98.196.51', type='string', help='IP address [default=39.98.196.51]')
-    # 添加数据包总参数 -c
-    parser.add_option('-c', '--count', dest='packetCount', default=5, type='int', help='Packet count [default = 5]')
-    # 添加保存文件名参数 -o
-    parser.add_option('-o', '--output', dest='fileName', default=f'{nowtime}.pcap', type='string', help=f'save filename [default = {nowtime}.pcap]')
-    (options, args) = parser.parse_args()
-    defFilter = "host " + options.hostIP
-
-    # 使用 PcapWriter 以追加模式打开文件
-    writer = PcapWriter(options.fileName, append=True, sync=True)
-
-    # 修改为无限循环捕获数据包
-    while True:
-        packets = sniff(filter=defFilter, prn=lambda x: PackCallBack(x, options.hostIP), count=options.packetCount)
-        # 追加保存输出文件
-        for packet in packets:
-            writer.write(packet)
+    app = QtWidgets.QApplication([])
+    window = PacketSnifferUI()
+    window.show()
+    app.exec_()
